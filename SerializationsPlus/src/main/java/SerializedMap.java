a99f10b93096df3902f71ba5108b98f5ec049fd6
ODINN555 <86920977+ODINN555@users.noreply.github.com>
2022-08-09 17:28:01 +0300

    added serializers
    added serializers:
    - ItemStack
    - List
    - HashMap
    removed unnecessary serailziers and added comments

--- SerializationsPlus/src/main/java/SerializedMap.java
+++ SerializationsPlus/src/main/java/SerializedMap.java
@@ -2,19 +2,41 @@ import java.io.Serializable;
 import java.util.Map;
 
 public class SerializedMap implements Serializable {
+    /**
+     * The values
+     */
     public Map<String,Object> Values;
+
+    /**
+     * The name of the serializer responsible for the values
+     */
     public String Name;
 
+    /**
+     *
+     * @param values given values
+     * @param name a given serializer name
+     */
     public SerializedMap(Map<String,Object> values,String name){
         this.Values = values;
         this.Name = name;
     }
 
+    /**
+     *
+     * @param values given values
+     * @param serializer a given class of serializer
+     */
     public SerializedMap(Map<String,Object> values,Class serializer){
         this.Values = values;
         this.Name = new Serializers().get(serializer).Name;
     }
 
+    /**
+     *
+     * @param values given values
+     * @param serializer a given serializer
+     */
     public SerializedMap(Map<String,Object> values,Serializer serializer){
         this.Values = values;
         this.Name = serializer.Name;
--- SerializationsPlus/src/main/java/Serializers.java
+++ SerializationsPlus/src/main/java/Serializers.java
@@ -1,10 +1,14 @@
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.material.MaterialData;
 import org.bukkit.potion.PotionEffectType;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Serializers {
     /**
@@ -16,35 +20,101 @@ public class Serializers {
         addDefaults();
     }
     public void addDefaults(){
-
-        serializers.add(new Serializer<Enchantment>("Enchantment",Enchantment.class) {
+        add(new Serializer<ItemStack>("ItemStack",ItemStack.class){
             @Override
-            public Map<String, Object> serializeValue(Enchantment object) {
+            public Map<String, Object> serializeValue(ItemStack object) {
                 Map<String,Object> map = new HashMap<>();
-                map.put("name",object.getName());
-                return map;
+               /* ItemMeta meta = object.getItemMeta();
+                map.put("name",meta.getDisplayName());
+                map.put("lore",meta.getLore());
+                map.put("count",object.getAmount());
+
+                Map<Enchantment,Integer> enchants = meta.getEnchants();
+                Map<byte[],Integer> serializedEnchants = new HashMap<>();
+                enchants.forEach((ench,lvl) ->
+                    serializedEnchants.put(Serializations.serialize(ench),lvl)
+                );
+                map.put("Enchants",serializedEnchants);
+                map.put("ItemFlags",meta.getItemFlags());
+                map.put("material",object.getType());
+                map.put("durability",object.getDurability());
+
+                return null;*/
+                return object.serialize();
             }
 
             @Override
-            public Enchantment deserializeValue(Map<String, Object> map) {
-                return Enchantment.getByName((String) map.get("name"));
+            public ItemStack deserializeValue(Map<String, Object> map) {
+                return ItemStack.deserialize(map);
             }
         });
-        serializers.add(new Serializer<PotionEffectType>("PotionEffectType",PotionEffectType.class) {
+        add(new Serializer<List>("List",List.class) {
             @Override
-            public Map<String, Object> serializeValue(PotionEffectType object) {
+            public Map<String, Object> serializeValue(List object) {
                 Map<String,Object> map = new HashMap<>();
-                map.put("name",object.getName());
+                List<byte[]> arr = new ArrayList<>();
+                for (Object o : object)
+                    arr.add(Serializations.serialize(o));
+                map.put("list",arr);
+
                 return map;
             }
 
             @Override
-            public PotionEffectType deserializeValue(Map<String, Object> map) {
-                return PotionEffectType.getByName((String) map.get("name"));
+            public List deserializeValue(Map<String, Object> map) {
+                List objects = new ArrayList();
+                List<byte[]> arr = (List<byte[]>) map.get("list");
+                for (byte[] bytes : arr)
+                    objects.add(Serializations.deserialize(bytes));
+
+                return objects;
+            }
+        });
+        add(new Serializer<HashMap>("Map",Map.class) {
+            @Override
+            public Map<String, Object> serializeValue(HashMap object) {
+                Map<String,Object> map = new HashMap<>();
+                Map serializedMap = new HashMap();
+                object.forEach((k,v) -> serializedMap.put(Serializations.serialize(k),Serializations.serialize(v)));
+                map.put("map",serializedMap);
+                return map;
+            }
+
+            @Override
+            public HashMap deserializeValue(Map<String, Object> map) {
+                HashMap deserializedMap = new HashMap();
+                Map objects = (Map) map.get("map");
+                objects.forEach((k,v) -> deserializedMap.put(Serializations.deserialize((byte[]) k),Serializations.deserialize((byte[]) v)));
+
+                return deserializedMap;
+            }
+        });
+        add(new Serializer<Location>("Location",Location.class) {
+            @Override
+            public Map<String, Object> serializeValue(Location object) {
+                Map<String,Object> map = new HashMap<>();
+                map.put("world",object.getWorld().getName());
+                map.put("X",object.getX());
+                map.put("y",object.getY());
+                map.put("z",object.getZ());
+                map.put("yaw",object.getYaw());
+                map.put("pitch",object.getYaw());
+                return map;
+            }
+
+            @Override
+            public Location deserializeValue(Map<String, Object> map) {
+                Location loc = new Location(Bukkit.getWorld((String) map.get("world")),(double) map.get("x"),(double) map.get("y"),(double) map.get("z"),(float)map.get("yaw"),(float) map.get("pitch"));
+                return loc;
             }
         });
     }
 
+    /**
+     *
+     * @param c a given class
+     * @return if the serializers contains a serializer which can serialize the given class
+     */
     public boolean contains(Class c){
         for (Serializer serializer : serializers)
             if(serializer.Type.isAssignableFrom(c))
@@ -53,6 +123,23 @@ public class Serializers {
             return false;
     }
 
+    /**
+     *
+     * @param c a given class
+     * @return if the serializers contains a serializer which can serialize the given class, and specifically it- not it's super class
+     */
+    public boolean containsExact(Class c){
+        for (Serializer serializer : serializers)
+            if(serializer.Type.equals(c))
+                return true;
+        return false;
+    }
+
+    /**
+     *
+     * @param name a given name
+     * @return if the serializers contains a serializer with the given name
+     */
     public boolean contains(String name){
         for (Serializer serializer : serializers)
             if(serializer.Name.equalsIgnoreCase(name))
@@ -61,15 +148,31 @@ public class Serializers {
             return false;
     }
 
+    /**
+     * adds the given serializer to the serializers, overrides if already exists.
+     * @param serializer a given serializer
+     */
     public void add(Serializer serializer){
-        serializers.add(serializer);
+        if(containsExact(serializer.Type))
+            serializers.remove(get(serializer.Type));
+
+            serializers.add(serializer);
     }
 
-    public Serializer get(Serializer c){
-        if(serializers.indexOf(c) == -1)
-            return null;
-        return serializers.get(serializers.indexOf(c));
+    /**
+     *
+     * @param index a given index
+     * @return the serializer at the given index
+     */
+    public Serializer get(int index){
+        return serializers.get(index);
     }
+
+    /**
+     *
+     * @param c a given class
+     * @return the most matching serializer for the given class, null if not found.
+     */
     public Serializer get(Class c){
         // for improved efficiency, get the matching serializers
         List<Serializer> matchingSerializers = new ArrayList<>();
@@ -94,6 +197,11 @@ public class Serializers {
         return null;
     }
 
+    /**
+     *
+     * @param name a given name
+     * @return the serializer with the given name, null if not found
+     */
     public Serializer get(String name){
         for (Serializer serializer : serializers)
             if(serializer.Name.equalsIgnoreCase(name))
